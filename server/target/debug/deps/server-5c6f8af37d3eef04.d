91e52c0bc21a642f66ca241e682e35555d9ab08a
Addison Chen <addisonc@untether.ai>
2023-05-24 20:06:46 -0400

    add gitignore

--- server/target/debug/deps/server-5c6f8af37d3eef04.d
+++
@@ -1,5 +0,0 @@
-C:\Ryerson\GitHub\sideproject\roach-roulette\server\target\debug\deps\server-5c6f8af37d3eef04.rmeta: src\main.rs
-
-C:\Ryerson\GitHub\sideproject\roach-roulette\server\target\debug\deps\server-5c6f8af37d3eef04.d: src\main.rs
-
-src\main.rs:
