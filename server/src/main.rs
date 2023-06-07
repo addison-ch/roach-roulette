6eb454896a9e42be3df8a0d317d7bcc26b8f5be7
Addison Chen <addisonchen.py@gmail.com>
2023-06-07 00:47:23 -0400

    add health_check endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case, non_camel_case_types, dead_code)]
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 mod routes;
 
@@ -9,12 +9,21 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().json("Hello from rust and mongoDB")
 }
 
+// Endpoint to be called to check on the status of the API
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Server running");
 
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("localhost", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
