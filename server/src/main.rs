3fe2f6c55f6cb6e3908c8f6f9b00d9e638357e80
Addison Chen <addisonchen.py@gmail.com>
2023-06-06 23:52:05 -0400

    format to pass CI

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,15 @@
-#![allow(non_snake_case,non_camel_case_types,dead_code)]
+#![allow(non_snake_case, non_camel_case_types, dead_code)]
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 mod routes;
 // The following fuctions are used to provide the basic nescessities of the game with the main function at the bottom.
 
-
 fn shuffle(deck: &[u8; 64]) -> [Vec<u8>; 4] {
-    let mut deck1 = Vec::new();
-    let mut deck2 = Vec::new();
-    let mut deck3 = Vec::new();
-    let mut deck4 = Vec::new();
+    let mut deck1: Vec<u8> = Vec::new();
+    let mut deck2: Vec<u8> = Vec::new();
+    let mut deck3: Vec<u8> = Vec::new();
+    let mut deck4: Vec<u8> = Vec::new();
 
     for a in 0..16 {
         deck4.push(deck[(a * 2) + 3]);
@@ -18,26 +17,26 @@ fn shuffle(deck: &[u8; 64]) -> [Vec<u8>; 4] {
         deck2.push(deck[(a * 2) + 1]);
         deck1.push(deck[a * 2]);
     }
-    println!("Deck1 : {:?}    Deck2 : {:?}    Deck3 : {:?}      Deck4 : {:?}", deck1,deck2,deck3,deck4);
+    println!(
+        "Deck1 : {:?}    Deck2 : {:?}    Deck3 : {:?}      Deck4 : {:?}",
+        deck1, deck2, deck3, deck4
+    );
     [deck1, deck2, deck3, deck4]
-
 }
 
+// fn deal(shuf: &[u8; 64]) -> [u8; 64]
+// {
+//     println!("Running-----------------------------------------");
 
-fn deal(shuf: &[u8; 64]) -> [u8; 64]
-{
-    println!("Running-----------------------------------------");
+//     // let mut value = shuf.clone();
+//     // let both: [Vec<u8>; 4] = shuffle(shuf, );
+//     // let deck1: &Vec<u8> = & both[0];
+//     // let deck2: &Vec<u8> = & both[1];
+//     // let deck3: &Vec<u8> = & both[2];
+//     // let deck4: &Vec<u8> = & both[3];
 
-    let mut value = shuf.clone();
-    let both = shuffle(shuf, );
-    let deck1 = & both[0];
-    let deck2 = & both[1];
-    let deck3 = & both[2];
-    let deck4 = & both[3];
-
-    *shuf
-}
-
+//     *shuf
+// }
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -52,5 +51,4 @@ async fn main() -> std::io::Result<()> {
         .bind(("localhost", 8080))?
         .run()
         .await
-
 }
--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,8 +1,7 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, HttpResponse, Responder};
 
 // planning to separate endpoints into this file, currently doesn't do anything
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
-
