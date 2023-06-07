b4863cda45f1110130431acea18f29116ddc43c3
Addison Chen <addisonchen.py@gmail.com>
2023-06-07 01:02:25 -0400

    refactor to allow integration tests

---
+++ server/src/lib.rs
@@ -0,0 +1,28 @@
+#![allow(non_snake_case, non_camel_case_types, dead_code)]
+
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+mod routes;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json("Hello from rust and mongoDB")
+}
+
+// Endpoint to be called to check on the status of the API
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub async fn run() -> Result<(), std::io::Error> {
+    println!("Server running");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,29 +1,9 @@
 #![allow(non_snake_case, non_camel_case_types, dead_code)]
 
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-mod routes;
+use server::run;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().json("Hello from rust and mongoDB")
-}
-
-// Endpoint to be called to check on the status of the API
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    println!("Server running");
-
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind(("localhost", 8080))?
-    .run()
-    .await
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    run().await
 }
