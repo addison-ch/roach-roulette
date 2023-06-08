cbb520e808f030b1c061ea59c034caae1508e884
addison <74084822+addison-ch@users.noreply.github.com>
2023-06-07 22:11:22 -0400

    Add health status endpoint (#2)
    * add health_check endpoint
    
    * refactor to allow integration tests
    
    * run cargo fmt

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,8 @@
 #![allow(non_snake_case, non_camel_case_types, dead_code)]
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use server::run;
 
-mod routes;
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().json("Hello from rust and mongoDB")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    println!("Server running");
-
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("localhost", 8080))?
-        .run()
-        .await
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    run().await
 }
