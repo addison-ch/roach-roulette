ae110f3f76be82fb8058d6b4f691587dacff19f2
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2023-06-07 23:01:58 -0400

    added endpoint to create a room and generate code

--- server/src/routes.rs
+++
@@ -1,7 +0,0 @@
-use actix_web::{get, HttpResponse, Responder};
-
-// planning to separate endpoints into this file, currently doesn't do anything
-#[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
