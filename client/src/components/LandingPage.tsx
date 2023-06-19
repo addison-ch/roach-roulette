affad0037f1487f5e0bce98e45df9a7e513771ea
Addison Chen <addisonchen.py@gmail.com>
2023-06-18 21:08:11 -0400

    style some buttons

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -4,16 +4,18 @@ import '../styles/LandingPage.css'
 
 const LandingPage: React.FC = () => {
     return (
-        <div className="flex flex-col items-center justify-evenly min-h-screen">
-            <h1 className="flex-1 bg-gray-200 p-4">Roach Roulette</h1>
+        <div className="flex flex-col items-center justify-evenly max-h-screen my-8 ">
+            <h1 className="flex-1 p-4 flex-grow ">Bug Bluff</h1>
             <img
                 src="roach.jpg"
                 alt="Cock Roach Poker"
-                className="flex-1 bg-gray-200 p-4"
+                className="flex-1  p-4 flex-grow"
             />
-            <div className="flex-1 p-4">
-                <Link className="" to="/create" >Create Room</Link>
-                <Link className="" to="/join" >Join Room</Link>
+            <div className="flex-1 w-5/6">
+                <div className="grid grid-cols-2 gap-6 p-4 w-full">
+                    <Link className="text-center border-primary border rounded-md border-solid bg-secondary p-3" to="/create" >Create Room</Link>
+                    <Link className="text-center border-primary border rounded-md border-solid bg-secondary p-3" to="/join" >Join Room</Link>
+                </div>
             </div>
         </div>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#e76f51",
+        secondary: "#f4a261",
+        thirdinary: "#e9c46a",
+        supporting: "#2a9d8f",
+        dark_supporting: "#264653",
+      },
+    },
   },
   plugins: [],
-}
-
+};
