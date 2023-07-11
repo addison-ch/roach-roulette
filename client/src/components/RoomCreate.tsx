120918bc9322d8dfbe7ec6288360f5e5a5c5ef65
addison <74084822+addison-ch@users.noreply.github.com>
2023-07-11 00:24:07 -0400

    Websocket connection from frontend (#9)
    * ws connection established
    
    * add json serialization and player list
    
    * remove unused file

--- client/src/components/RoomCreate.tsx
+++ client/src/components/RoomCreate.tsx
@@ -1,22 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios, { AxiosError } from 'axios';
 
-// Define the shape of the data you expect to receive from the API
 interface ApiResponse {
-    // Add the properties you expect to receive
-    id: number;
-    name: string;
-    // etc...
+    code: string;
 }
-const RoomCreate: React.FC = () => {
 
+const RoomCreate: React.FC = () => {
+    const ws = useRef<WebSocket | null>(null);
     const [data, setData] = useState<ApiResponse | null>(null);
     const [error, setError] = useState<string | null>(null);
 
+    const ws_start = (code: string) => {
+        // Create a WebSocket instance
+        if (code === null) {
+            throw new Error('Room code cant be null')
+        }
+        console.log(code)
+        ws.current = new WebSocket(`ws://127.0.0.1:3005/start_connection/${code}`);
+
+        // Add an onOpen event listener to the WebSocket instance
+        ws.current.onopen = () => {
+            console.log('WebSocket connection established');
+        };
+
+        // Add an onMessage event listener to the WebSocket instance
+        ws.current.onmessage = (event) => {
+            console.log(event)
+            console.log('Received message:', event.data);
+        };
+
+        // Add an onClose event listener to the WebSocket instance
+        ws.current.onclose = () => {
+            console.log('WebSocket connection closed');
+        };
+
+        ws.current.onerror = (event) => {
+            setError('WebSocket connection error');
+            console.error('WebSocket connection error:', event);
+        };
+
+
+        // Clean up the WebSocket instance on unmount
+        return () => {
+            if (ws.current) {
+                ws.current.close();
+            }
+        };
+    }
+
     const handleClick = async () => {
         try {
-            const response = await axios.get<ApiResponse>('http://127.0.0.1:3005/create_room');
-            setData(response.data);
+            const res = await axios.get<ApiResponse>('http://127.0.0.1:3005/create_room');
+            console.log(res.data.code)
+            ws_start(res.data.code);
+            setData(res.data);
         } catch (error) {
             const axiosError = error as AxiosError;
             // Handle the error based on the response from the server
--- server/src/messages.rs
+++ server/src/messages.rs
@@ -1,4 +1,5 @@
 use actix::prelude::{Message, Recipient};
+use serde::Serialize;
 use uuid::Uuid;
 
 #[derive(Message)]
@@ -24,6 +25,49 @@ pub struct Disconnect {
 #[rtype(result = "()")]
 pub struct ClientActorMessage {
     pub id: Uuid,
+    pub msg: String,
+    pub room_id: Uuid,
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct PlayerListMessage {
+    pub id: Uuid,
+    pub room_id: Uuid,
+}
+#[derive(Message, Serialize)]
+#[rtype(result = "()")]
+pub struct OutboundPlayerListMessage {
+    #[serde(rename = "type")]
+    pub event_type: String,
+    pub users: Vec<Uuid>,
+}
+
+#[derive(Message, Serialize)]
+#[rtype(result = "()")]
+pub struct OutboundWelcomeMessage {
+    #[serde(rename = "type")]
+    pub event_type: String,
+    pub id: Uuid,
+    pub msg: String,
+    pub room_id: Uuid,
+}
+
+#[derive(Message, Serialize)]
+#[rtype(result = "()")]
+pub struct OutboundJoinMessage {
+    #[serde(rename = "type")]
+    pub event_type: String,
+    pub id: Uuid,
+    pub msg: String,
+    pub room_id: Uuid,
+}
+
+#[derive(Message, Serialize)]
+#[rtype(result = "()")]
+pub struct OutboundDisconnectMessage {
+    #[serde(rename = "type")]
+    pub event_type: String,
     pub msg: String,
     pub room_id: Uuid,
 }
