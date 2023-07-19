1ecf7a97d64544b4d39c333a856cf78be4860a11
Douglas Huang <74084786+douglashuangg@users.noreply.github.com>
2023-07-18 23:36:31 -0400

    Dhuang/drag and drop cards (#10)
    * fake change
    
    * created lobby
    
    * room rough
    
    * clippy
    
    * fix failing test
    
    * formatting
    
    * drag and drop functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import LandingPage from './components/LandingPage';
-import RoomCreate from './components/RoomCreate';
-import RoomJoin from './components/RoomJoin';
-import './App.css';
+import LandingPage from "./components/LandingPage";
+import RoomCreate from "./components/RoomCreate";
+import RoomJoin from "./components/RoomJoin";
+import GameBoard from "./components/GameBoard";
+import "./App.css";
 
 function App(): JSX.Element {
-
   return (
     <>
       <div className="flex flex-row items-center justify-center min-h-screen">
@@ -15,13 +15,13 @@ function App(): JSX.Element {
             <Route path="/" element={<LandingPage />} />
             <Route path="/create" element={<RoomCreate />} />
             <Route path="/join" element={<RoomJoin />} />
+            <Route path="/game" element={<GameBoard />} />
             <Route path="*" element={<h1>Not Found</h1>} />
           </Routes>
         </Router>
       </div>
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/GameBoard.tsx
@@ -0,0 +1,148 @@
+import React, { useState, useReducer, useRef } from "react";
+import {
+  motion,
+  animate,
+  AnimatePresence,
+  useMotionValue,
+} from "framer-motion";
+import "../styles/GameBoard.css";
+// import { DndContext } from "@dnd-kit/core";
+// import { Draggable, Droppable, DragDropContext } from "react-beautiful-dnd";
+// import { StrictModeDroppable } from "./Droppable";
+import { DndContext } from "@dnd-kit/core";
+
+import { Droppable } from "./Droppable";
+import { Draggable } from "./Draggable";
+const finalSpaceCharacters = [
+  {
+    id: "gary",
+    name: "Gary Goodspeed",
+    thumb: "/images/gary.png",
+  },
+  {
+    id: "cato",
+    name: "Little Cato",
+    thumb: "/images/cato.png",
+  },
+  {
+    id: "kvn",
+    name: "KVN",
+    thumb: "/images/kvn.png",
+  },
+  {
+    id: "mooncake",
+    name: "Mooncake",
+    thumb: "/images/mooncake.png",
+  },
+  {
+    id: "quinn",
+    name: "Quinn Ergon",
+    thumb: "/images/quinn.png",
+  },
+];
+
+const GameBoard: React.FC = () => {
+  const [selectedCard, setSelectedCard] = useState(10);
+  const list = ["🕷", "🦇", "🐸", "🐀", "🐜", "🐜", "🐜", "🐜"];
+
+  const handleCardClick = (index: number) => {
+    setSelectedCard(index);
+    console.log(index);
+  };
+
+  const handleBlur = () => {
+    setSelectedCard(-1);
+    console.log("BLURRED");
+  };
+
+  //drag kit
+  const [cards, setCards] = useState<string[]>([
+    "🕷",
+    "🦇",
+    "🐸",
+    "🐀",
+    "🐜",
+    "🐜",
+    "🐜",
+    "🐜",
+  ]);
+  const [yourCardMat, setYourCardMat] = useState<string[]>([]);
+
+  function handleDragEnd(event: any) {
+    const { over } = event;
+    if (!over) return;
+    if (over.id == "A") {
+      setYourCardMat([...yourCardMat, cards[selectedCard]]);
+      cards.splice(selectedCard, 1);
+      setCards(cards);
+    }
+  }
+
+  return (
+    <>
+      <DndContext onDragEnd={handleDragEnd}>
+        <div style={{ position: "absolute", top: 0 }}>
+          <Droppable key="A" id="A">
+            {yourCardMat
+              .sort((a, b) => a.localeCompare(b))
+              .map((card, index) => {
+                return (
+                  <Draggable
+                    key={"yourCardMat" + index}
+                    id={"yourCardMat" + index}
+                    disabled={true}
+                    style={{ cursor: "default" }}
+                  >
+                    {card}
+                  </Draggable>
+                );
+              })}
+          </Droppable>
+        </div>
+        <div>
+          <Droppable key="D" id="D" style={{ height: "200px", width: "800px" }}>
+            {cards.map((card, index) => {
+              let zValue: any;
+              if (index > selectedCard) {
+                zValue = list.length - index;
+                console.log("VALUE", index, zValue);
+              } else if (index < selectedCard) {
+                zValue = index;
+                console.log("NOT VALUE", index);
+              }
+              return (
+                <Draggable key={"yourHand" + index} id={"yourHand" + index}>
+                  <div
+                    style={{
+                      display: "flex",
+                      zIndex: `${zValue}`,
+                    }}
+                  >
+                    <motion.div
+                      style={{
+                        marginLeft: "-80px",
+                        border: `${
+                          index === selectedCard ? "1px solid black" : "none"
+                        }`,
+                      }}
+                      onClick={() => handleCardClick(index)}
+                      onMouseEnter={() => handleCardClick(index)}
+                      whileHover={{ scale: 1.1, zIndex: list.length }}
+                      className="card"
+                      // tabIndex={0}
+                      onBlur={handleBlur}
+                    >
+                      {card}
+                    </motion.div>
+                  </div>
+                </Draggable>
+              );
+            })}
+          </Droppable>
+        </div>
+      </DndContext>
+    </>
+  );
+};
+
+export default GameBoard;
